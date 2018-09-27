e9524232aabb0fd3996df273e6a39aff6f422aeb
Michael <michael.craun@gmail.com>
2018-09-27 19:09:01 -0400

    Initial Commit

---
+++ Dice Calc Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice Calc Test
+//
+//  Created by Michael Craun on 9/27/18.
+//  Copyright © 2018 CraunicProductions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
